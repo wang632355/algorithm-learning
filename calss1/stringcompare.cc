7ece31c4636958a0d34c2772e942485d1d4c3f9d
gang.wang <gang.wang@deptrum.com>
2022-11-26 15:43:06 +0800

    ss

---
+++ calss1/stringcompare.cc
@@ -0,0 +1,18 @@
+char *StrStr(const char *str, const char *target) {
+  if (!*target) {
+    return const_cast<char *>(str);
+  }
+  char *p1 = (char *)str;
+  while (*p1) {
+    char *p1_begin = p1, *p2 = (char *)target;
+    while (*p1 && *p2 && *p1 == *p2) {
+      p1++;
+      p2++;
+    }
+    if (!*p2) {
+      return p1_begin;
+    }
+    p1 = p1_begin + 1;
+  }
+  return nullptr;
+}
